79aa697fc677cde8d8127975ad4f34887f830316
Brendo Almeida <72768968+brandonspecter1618@users.noreply.github.com>
2023-10-10 15:54:44 -0300

    Add files via upload

---
+++ Project + SITE + HTML + CSS + PHP/PHP/PHPbkp/php/phpTest.php
@@ -0,0 +1,108 @@
+<?php
+
+function phpTest(){
+    // Função de tratamento de erro personalizada para converter erros em respostas JSON
+
+    $login    = $_POST['key1'];
+    $password = $_POST['key2'];
+    $drop     = $_POST['key3'];  
+    $options  = $_POST['key4'];
+
+
+    $db    = "db_users";
+    $host  = "127.0.0.1";
+    $user  = "postgres";
+    $pass  = "22091992";
+
+    $connection = pg_connect("host=$host dbname=$db user=$user password=$pass");
+    $status     = pg_connection_status($connection); 
+
+        if($options === 'conn'){
+            if($status === PGSQL_CONNECTION_OK){
+                echo("Sucessfully connection" ."<br>".pg_dbname());
+            }else{
+                echo("<br>Fail Connection" .pg_last_error() ."<br><br>");
+            }
+
+        }else
+            if($options === 'insert'){  //Se retirar as aspas aparece um erro no PHP
+                $queryInsert = "INSERT into aluno(login, password) values('$login', '$password');";
+                $query = "SELECT * FROM aluno WHERE login='$login' AND password='$password'";
+
+                $dbSelectQuery = pg_query($connection, $query);
+                    
+                    if(pg_num_rows($dbSelectQuery) > 0){
+                        echo("Já existe um usuário cadastrado");
+                    }else{
+                        $dbConnQuery = pg_query($connection, $queryInsert);
+                            if($dbConnQuery){
+                                echo("Insert Data Sucess");
+                            }else{
+                                echo("Failed Insert Data");
+                            }
+                }   
+
+            }else
+                if($options === 'select') {
+                    $query = "SELECT * FROM aluno";
+                    $dbConnQuery = pg_query($connection, $query);
+                
+                    echo '<table style="border-collapse: collapse; width: 100%;">';
+                    echo '<tr>';
+                    echo '<th style="padding: 30px; border: 5px solid #000;">ID</th>';
+                    echo '<th style="padding: 30px; border: 5px solid #000;">Login</th>';
+                    echo '<th style="padding: 30px; border: 5px solid #000;">Password</th>';
+                    echo '</tr>';
+                
+                    while ($row = pg_fetch_row($dbConnQuery)) {
+                        echo '<tr>';
+                        echo '<td style="padding: 30px; border: 5px solid #000;">' . $row[0] . '</td>';
+                        echo '<td style="padding: 30px; border: 5px solid #000;">' . $row[1] . '</td>';
+                        echo '<td style="padding: 30px; border: 5px solid #000;">' . $row[2] . '</td>';
+                        echo '</tr>';
+                    }
+                
+                    echo '</table>';
+
+                }else
+                    if($options === 'drop'){
+                        $query = "DELETE FROM aluno WHERE id = $drop";
+                        $dbConnQuery = pg_query($connection, $query);
+                        $rowsAffected = pg_affected_rows($dbConnQuery);
+
+                            if($rowsAffected > 0){
+                                echo("Foi apagado o ID: " . $drop);
+                            }else{
+                                echo("Não foi apagado");
+                            }
+                    }
+                    else   
+                        if($options === 'login'){
+                            $query = "SELECT * FROM aluno WHERE login='$login' AND password='$password'";
+                            $dbConnQuery = pg_query($connection, $query);
+
+                                if(pg_num_rows($dbConnQuery) > 0){
+                                    echo 'Login Com Sucesso' .'<br><br>';
+
+                                    echo '<table style="border-collapse: collapse; width: 100%;">';
+                                    echo '<th style="padding: 30px; border: 5px solid #000;">Login</th>';
+                                    echo '<th style="padding: 30px; border: 5px solid #000;">Password</th>';
+
+                                    while ($row = pg_fetch_row($dbConnQuery)) {
+                                        echo '<tr>';
+                                        echo '<td style="padding: 30px; border: 5px solid #000;">' . $row[1] . '</td>';
+                                        echo '<td style="padding: 30px; border: 5px solid #000;">' . $row[2] . '</td>';
+                                        echo '</tr>';
+                                    }
+                                    echo '</table>';
+
+                                }else{
+                                    echo 'Usuário ou Senha incorreto';
+                                }
+
+        }
+        
+    pg_close($connection);
+}
+phpTest()
+?>
